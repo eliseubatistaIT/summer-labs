d6b56d4ac2496b21631ef2012fb28a2922451042
Guilherme Martins <guissmartins2013@gmail.com>
2023-07-05 11:00:52 +0100

    Challenge 1 concluído

---
+++ src/screens/challenge1/cabecalho.tsx
@@ -0,0 +1,24 @@
+// Referência: https://bobbyhadz.com/blog/react-optional-props-typescript
+
+export interface CabecalhoProps {
+    titulo: string; // 👈️ required (no question mark)
+    subtitulo?: string// 👈️ marked optional
+}
+
+export default function Cabecalho({titulo, subtitulo} : CabecalhoProps) {
+    if (subtitulo) { // se contém título
+        return (
+            <div>
+              <h2>{titulo}</h2>
+              <h4>{subtitulo}</h4>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div>
+              <h2>{titulo}</h2>
+            </div>
+        );
+    }
+}
--- src/screens/challenge1/challenge1.tsx
+++ src/screens/challenge1/challenge1.tsx
@@ -1,16 +1,22 @@
 import { AppScreen, CustomButton } from "@components";
 import { useChallenge1Helper } from "./challenge1.hook";
+import Cabecalho from "./cabecalho"
+import { CabecalhoProps } from "./cabecalho";
 
 export const Challenge1 = () => {
-  const { sayHello } = useChallenge1Helper();
-
-  return (
-    <AppScreen header={{ title: "Challenge 1" }}>
-      <CustomButton
+  // const { sayHello } = useChallenge1Helper();
+  /* (estava antes como clid de AppScreen)
+  <CustomButton
         text="Say Hello"
         onClick={() => sayHello()}
         styles={{ margin: "auto auto 20px auto" }}
       />
+  */
+
+  return (
+    <AppScreen header={{ title: "Challenge 1" }}>
+      <Cabecalho titulo={"Desafio concluído"} subtitulo={"Com subtítulo"}/>
+      <Cabecalho titulo={"Só para mostrar que o subtítulo pode ser facultativo!"}/>
     </AppScreen>
   );
 };
