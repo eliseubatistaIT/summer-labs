a38fdd04678127d8e93e585462fdfbb18165e3c1
Guilherme Martins <guissmartins2013@gmail.com>
2023-07-10 14:24:37 +0100

    Avanços...

--- src/screens/project/project.tsx
+++ src/screens/project/project.tsx
@@ -1,16 +1,49 @@
 import { AppScreen, CustomButton } from "@components";
 import { useProjectHelper } from "./project.hook";
+import React, { useState } from "react";
+
+interface UserRequest {
+  limit: number;
+  offset: number;
+}
+
+
+interface PokemonListResultItem {
+  name: string;
+  url: string;
+}
+
+interface PokemonListResult {
+  results: PokemonListResultItem[];
+}
 
 export const Project = () => {
+  // Saber se o utilizador já fez algum pedido
+  const [withRequest, setRequest] = useState(false); // no inicio ainda não fez um pedido
+
   const { sayHello } = useProjectHelper();
 
+  const fetchPokemons = async () => {
+    const fetchResult = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const fetchJSON = await fetchResult.json();
+    //console.log(fetchJSON);
+    const pokemonsData = [];
+    for (let i = 0; i < fetchJSON.results.length; i++) {
+      // console.log(fetchJSON.results[i]);
+      const pokeResult = await fetch(fetchJSON.results[i].url);
+      const pokeJSON = await pokeResult.json();
+      pokemonsData.push(pokeJSON); // CUIDADO, só se recebem 20 pokemons de cada vez
+    }
+    console.log(pokemonsData);
+  };
+
+  React.useEffect(() => {
+    fetchPokemons();
+  }, []);
+
   return (
-    <AppScreen header={{ title: "Project" }}>
-      <CustomButton
-        text="Say Hello"
-        onClick={() => sayHello()}
-        styles={{ margin: "auto auto 20px auto" }}
-      />
+    <AppScreen header={{ title: "Project - Poke API - REACT" }}>
+      
     </AppScreen>
   );
 };
