94f02939e69350ddf6ef9911f0eb248bb19cc8be
Guilherme Martins <guissmartins2013@gmail.com>
2023-07-11 11:48:01 +0100

    Conteudos PokeAPI

--- src/screens/project/Pokemon.tsx
+++ src/screens/project/Pokemon.tsx
@@ -1,50 +1,78 @@
 interface Ability {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 }
 
 interface Stats {
-    hp: number;
-    attack: number;
-    defense: number;
-    special_attack: number; // special-attack
-    special_defense: number; // special-defense
-    speed: number;
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number; // special-attack
+  special_defense: number; // special-defense
+  speed: number;
 }
 
 interface Sprites {
-    back_default: string;
-    back_female ?: string;
-    back_shiny: string;
-    back_shiny_female ?: string;
-    front_default: string;
-    front_female ?: string
-    front_shiny: string
-    front_shiny_female ?: string
+  back_default: string;
+  back_female?: string;
+  back_shiny: string;
+  back_shiny_female?: string;
+  front_default: string;
+  front_female?: string;
+  front_shiny: string;
+  front_shiny_female?: string;
 }
 
 export interface Pokemon {
-    abilities: Ability[];
-    name: string;
-    index: number; // número do pokemon na pokedex
-    sprites: Sprites; // criar estrutura das sprites
-    stats: Stats;
-    types: string[];
+  abilities: Ability[];
+  name: string;
+  index: number; // número do pokemon na pokedex
+  sprites: Sprites; // criar estrutura das sprites
+  stats: Stats;
+  types: string[];
 }
 
-export function DrawAllSprites({sprites}: {sprites: Sprites}) {
-    return (
-        <>
-            <img></img>
-        </>
-    );
+export function DrawAllSprites({ sprites }: { sprites: Sprites }) {
+  return ( // perguntar sobre as imagens facultativas
+    <>
+      <div>
+        <img
+            src = {sprites.back_default}
+            alt = "Back-Default"
+        />
+        <img
+            src = {sprites.back_female}
+            alt = "Back-Female"
+        />
+      </div>
+      <img
+        src = {sprites.back_default}
+        alt = "Back-Default"
+      />
+      <img
+        src = {sprites.back_default}
+        alt = "Back-Default"
+      />
+    </>
+  );
 }
 
 export function DrawPokemon(pokemon: Pokemon) {
+  return (
     <>
-      <label>
-        Pokemon Name: {pokemon.name}
-      </label>
-      <DrawAllSprites sprites={pokemon.sprites}/>
+        <label>
+        Pokemon Name: {pokemon.name}; Nº: {pokemon.index}
+        </label>
+        <DrawStats stats={pokemon.stats}/>
+        <DrawAllSprites sprites={pokemon.sprites} />
     </>
+  );
+}
+
+function DrawStats({ stats } : {stats: Stats}) {
+    return(
+        <>
+
+        </>
+    );
 }
--- src/screens/project/project.tsx
+++ src/screens/project/project.tsx
@@ -1,6 +1,7 @@
 import { AppScreen, CustomButton } from "@components";
 import { useProjectHelper } from "./project.hook";
 import React, { useState } from "react";
+import { DrawPokemon } from "./Pokemon";
 
 interface PokemonListResultItem {
   name: string;
@@ -17,17 +18,36 @@ export const Project = () => {
   // Início do programa - mostrar os pokemons em sequência
   const [initial, setInitial] = useState(true);
   // Armazenados valores de pedido do utilizador
-  const [limit, setLimit] = useState(0);
-  
+  const [limit, setLimit] = useState<number>(20); // nº de pokemons que vai buscar
+  const [offset, setOffset] = useState<number>(0); // indice donde começa
+
+  const pokemonsData = []; // Dados de todos os pokemons desejados
+
+  /* CUIDADOS a ter
+     - só 
+  */
 
   const { sayHello } = useProjectHelper();
 
+  function handleLimitChange(e: any) {
+    let n = e.target.valueAsNumber;
+    if (n <= 20 || n >= 0) {
+      setLimit(e.target.valueAsNumber);
+    }
+  }
 
-  const fetchPokemons = async () => {
+  function handleOffsetChange(e: any) {
+    let n = e.target.valueAsNumber;
+    if (n > 1280) {
+
+    }
+    setOffset(e.target.valueAsNumber);
+  }
+
+  const fetchPokemons = async (limit: number, offset: number) => {
     const fetchResult = await fetch("https://pokeapi.co/api/v2/pokemon");
     const fetchJSON = await fetchResult.json();
     //console.log(fetchJSON);
-    const pokemonsData = [];
     for (let i = 0; i < fetchJSON.results.length; i++) {
       // console.log(fetchJSON.results[i]);
       const pokeResult = await fetch(fetchJSON.results[i].url);
@@ -38,12 +58,36 @@ export const Project = () => {
   };
 
   React.useEffect(() => {
-    fetchPokemons();
+    fetchPokemons(limit, offset);
   }, []);
 
   return (
     <AppScreen header={{ title: "Project - Poke API - REACT" }}>
-      
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+        alt="PokeAPI"
+        max-width="50%"
+      />
+      <p></p>
+      <h2>Only 20 pokemons are shown at the same time</h2>
+      <p>Scroll through the page to see more pokemon</p>
+      <p>
+        OR Search write in the 2 spaces below to search for the pokemons you
+        want
+      </p>
+      <div>
+        <label>
+          (Number of pokemons you want to get; <i>MAXIMUM = 20</i>) Limit:
+          <input type="number" value={limit} onChange={handleLimitChange} />
+          <br></br>
+        </label>
+        <label>
+          (From which index you want to search for; <i>MAXIMUM = 1280</i>) Offset:
+          <input type="number" value={offset} onChange={handleOffsetChange} />
+          <br></br>
+        </label>
+      </div>
+      <button>See results</button>
     </AppScreen>
   );
 };
