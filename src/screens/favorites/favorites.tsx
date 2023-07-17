043926a789844e2bea8cdb915399a6b8fb05b2e1
Guilherme Martins <guissmartins2013@gmail.com>
2023-07-17 11:54:58 +0100

    Ecrão Favoritos

--- src/screens/favorites/favorites.tsx
+++ src/screens/favorites/favorites.tsx
@@ -1,3 +1,13 @@
+import { AppScreen } from "@components";
+
 export const FavoritesScreen = () => {
-    return();
+    return(
+        <AppScreen header={{ title: "Pokemon Favourites" }}>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+          alt="PokeAPI"
+          width="60%"
+        />
+      </AppScreen>
+    );
 }
